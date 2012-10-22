f9a358fcd888d983a0861877f275111da735db53
chundaizi <chundaizi@gmail.com>
2012-10-22 15:33:18 +0000

    Issue 4:	实现表格中的下拉选择框

---
+++ trunk/WebContent/medicalrecord/data/dics/dic-ICU.js
@@ -0,0 +1,10 @@
+//重症监护室代码
+var dic_ICU = [
+{"value":"","text":""},
+{"value":"01","text":"CCU - 心脏监护室"},
+{"value":"02","text":"RICU - 呼吸监护室"},
+{"value":"03","text":"SICU - 外科监护室"},
+{"value":"04","text":"NICU - 新生儿监护室"},
+{"value":"05","text":"PICU - 儿科监护室"},
+{"value":"99","text":"其他"}
+]

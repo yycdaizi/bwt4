2261a5afd8e76f0c6f741b6ae29a0744dd88e3e1
chundaizi <chundaizi@gmail.com>
2012-10-30 12:56:22 +0000

    Issue 10:	数据库的各项信息应改成可用xml或property文件配置的

--- trunk/src/org/beiyi/SysInfo.java
+++ trunk/src/org/beiyi/SysInfo.java
@@ -1,12 +1,15 @@
 package org.beiyi;
 
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 import org.beiyi.wt4.MappingXMLOperator;
 
 public class SysInfo {
 	private static Map<String, MappingXMLOperator> map;
+	private static Properties config;
 	static{
 		map = new HashMap<String, MappingXMLOperator>();
 		map.put("medicalRecord", new MappingXMLOperator("/org/beiyi/wt4/mapping/MedicalRecord.Mapping.xml"));
@@ -15,9 +18,26 @@ public class SysInfo {
 		map.put("operator", new MappingXMLOperator("/org/beiyi/wt4/mapping/Operation.Mapping.xml"));
 		map.put("disabled", new MappingXMLOperator("/org/beiyi/wt4/mapping/Disabled.Mapping.xml"));
 		map.put("icu", new MappingXMLOperator("/org/beiyi/wt4/mapping/Icu.Mapping.xml"));
+		
+		// 生成输入流  
+        InputStream ins=SysInfo.class.getResourceAsStream("/cfg/config.properties");  
+        // 生成properties对象  
+        config = new Properties();  
+        try {  
+        	config.load(ins);  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }
 	}
 
 	public static MappingXMLOperator getMappingXMLOperator(String name){
 		return map.get(name); 
+	}
+	
+	public static String getParmeter(String name){
+		if(config!=null){
+			return config.getProperty(name);
+		}
+		return null;
 	}
 }

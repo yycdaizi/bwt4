cf5c5d29cb8f5182fdf4081d25084b80b70a24dc
chundaizi <chundaizi@gmail.com>
2012-11-24 11:29:53 +0000

    Issue 25: 校验不通过时在该项的tab页上显示提示。 添加了tab1,tab2,tab4,tab5校验不通过在标签上显示提示的功能

--- branches/b_wt4_server/trunk/WebContent/medicalrecord/js/CaseXmlExport.js
+++ branches/b_wt4_server/trunk/WebContent/medicalrecord/js/CaseXmlExport.js
@@ -2,13 +2,86 @@
  * 校验form
  */
 function checkValid(){
-    var isvalid1 = $("#formTab1").form('validate');
-    var isvalid2 = $("#formTab2").form('validate');
+    var isvalid1 = validateTab1();
+    var isvalid2 = validateTab2();
     var isvalid3 = $("#surgery").form('validate');
-    var isvalid4 = $("#formTab4").form('validate');
-    var isvalid5 = $("#formTab5").form('validate');
+    var isvalid4 = validateTab4();
+    var isvalid5 = validateTab5();
     var isvalid = (isvalid1 && isvalid2 && isvalid3 && isvalid4 && isvalid5);
 	return isvalid;
+}
+//校验tab1
+function validateTab1(){
+	var validform = $("#formTab1").form('validate');
+	var mainTabs = $('#mainTabs');
+	if(validform){
+		updateTabIcon(mainTabs,'患者基本信息','');
+	}else{
+		updateTabIcon(mainTabs,'患者基本信息','icon-warn');
+	}
+	return validform;
+}
+
+//校验tab2
+function validateTab2(){
+	var validform = $("#formTab2").form('validate');
+	var validgrid = validateGrid('ABDS');
+	var valid = validform&&validgrid;
+	var mainTabs = $('#mainTabs');
+	if(valid){
+		updateTabIcon(mainTabs,'诊断信息','');
+	}else{
+		updateTabIcon(mainTabs,'诊断信息','icon-warn');
+	}
+	return valid;
+}
+
+//校验tab4
+function validateTab4(){
+	var validform = $("#formTab4").form('validate');
+	var validgrid = validateGrid('AEKS')&&validateGrid('AENS');
+	var valid = validform&&validgrid;
+	var mainTabs = $('#mainTabs');
+	if(valid){
+		updateTabIcon(mainTabs,'其他医疗信息','');
+	}else{
+		updateTabIcon(mainTabs,'其他医疗信息','icon-warn');
+	}
+	return valid;
+}
+//校验tab5
+function validateTab5(){
+	var validform = $("#formTab5").form('validate');
+	var mainTabs = $('#mainTabs');
+	if(validform){
+		updateTabIcon(mainTabs,'费用信息','');
+	}else{
+		updateTabIcon(mainTabs,'费用信息','icon-warn');
+	}
+	return validform;
+}
+
+//校验表格中的所有数据
+function validateGrid(id){
+	var $grid = $("#"+id);
+	var valid = true;
+	for(var i=0;i<$grid.datagrid('getRows').length;i++){
+		if(!$grid.datagrid('validateRow', i)){
+			valid = false;
+			$grid.datagrid('beginEdit', i);
+		}
+	}
+	return valid;
+}
+
+function updateTabIcon(tabs,title,iconCls){
+	var tab = tabs.tabs('getTab',title);
+	tabs.tabs('update', {
+		tab: tab,
+		options:{
+			iconCls:iconCls
+		}
+	});
 }
 
 function caseXmlExportClick(){

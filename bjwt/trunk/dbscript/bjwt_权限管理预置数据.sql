53a066e6989e155ed9e176204fa05a23c6156a29
kexinwb <kexinwb@gmail.com>
2013-02-01 17:05:46 +0000

    更新预置数据

--- bjwt/trunk/dbscript/bjwt_权限管理预置数据.sql
+++ bjwt/trunk/dbscript/bjwt_权限管理预置数据.sql
@@ -1,1 +1,5 @@
-insert into b_menu (menuname,menuurl,menuicon) values ('','','');
+insert into b_menu (menuid, menuname, menuurl, menuicon, ts) values (1, '菜单管理', 'menumng.jsp', 'icon icon-nav', '2013-02-02 00:50:13');
+insert into b_menu (menuid, menuname, menuurl, menuicon, ts) values (2, '角色管理', 'rolemng.jsp', 'icon icon-nav', '2013-02-02 00:49:42');
+insert into b_menu (menuid, menuname, menuurl, menuicon, ts) values (3, '用户管理', 'usermng.jsp', 'icon icon-nav', '2013-02-02 00:49:51');
+insert into b_menu (menuid, menuname, menuurl, menuicon, ts) values (4, '机构管理', 'orgmng.jsp', 'icon icon-nav', '2013-02-02 00:49:57');
+insert into b_menu (menuid, menuname, menuurl, menuicon, ts) values (5, '权限管理', 'permissionmng.jsp', 'icon icon-nav', '2013-02-02 00:50:05');

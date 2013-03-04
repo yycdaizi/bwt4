d4038643121d27db3e52e0edfd926df94187d50e
kexinwb@gmail.com <kexinwb@gmail.com@ea115d3b-e248-290a-2b54-6fe015b9cb3d>
2013-03-04 12:29:13 +0000

    角色管理

--- bjwt/trunk/src/main/java/org/bjdrgs/bjwt/authority/model/Role.java
+++ bjwt/trunk/src/main/java/org/bjdrgs/bjwt/authority/model/Role.java
@@ -6,8 +6,13 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
@@ -23,26 +28,27 @@ public class Role implements Serializable {
 	@Column(name = "roleid", nullable = false, unique = true)
 	private Integer roleid;
 
-	@NotBlank(message = "{common.text.notBlank}")
-	@Length(max = 0, message = "{common.length.limit}")
-	@Column(name = "orgid", nullable = false, length = 0)
-	private Integer orgid;
-   
+	@JsonIgnore
+	@ManyToOne
+	@Cascade({ CascadeType.REFRESH })
+	@JoinColumn(name = "orgid")
+	private Org org;
+
 	@Length(max = 150, message = "{common.length.limit}")
 	@Column(name = "rolecode", nullable = false, length = 150)
 	private String rolecode;
-   
+
 	@Length(max = 150, message = "{common.length.limit}")
 	@Column(name = "rolename", nullable = false, length = 150)
 	private String rolename;
-   
+
 	@Length(max = 3000, message = "{common.length.limit}")
 	@Column(name = "note", nullable = false, length = 3000)
 	private String note;
-	
+
 	@Column(name = "ts")
 	private String ts;
-    
+
 	public Integer getRoleid() {
 		return roleid;
 	}
@@ -50,15 +56,15 @@ public class Role implements Serializable {
 	public void setRoleid(Integer roleid) {
 		this.roleid = roleid;
 	}
-   
-	public Integer getOrgid() {
-		return orgid;
+
+	public Org getOrg() {
+		return org;
 	}
 
-	public void setOrgid(Integer orgid) {
-		this.orgid = orgid;
+	public void setOrg(Org org) {
+		this.org = org;
 	}
-   
+
 	public String getRolecode() {
 		return rolecode;
 	}
@@ -66,7 +72,7 @@ public class Role implements Serializable {
 	public void setRolecode(String rolecode) {
 		this.rolecode = rolecode;
 	}
-   
+
 	public String getRolename() {
 		return rolename;
 	}
@@ -74,7 +80,7 @@ public class Role implements Serializable {
 	public void setRolename(String rolename) {
 		this.rolename = rolename;
 	}
-   
+
 	public String getNote() {
 		return note;
 	}
@@ -90,7 +96,13 @@ public class Role implements Serializable {
 	public void setTs(String ts) {
 		this.ts = ts;
 	}
-	
-   
-}
 
+	// view prop
+	public String getOrg_orgid(){
+		return org != null ? org.getOrgid().toString() : null;
+	}
+	public String getOrg_orgname() {
+		return org != null ? org.getOrgname() : null;
+	}
+
+}
